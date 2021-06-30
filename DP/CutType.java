b6ae051ed54d934d3864ff6e7fdaf3f2504e2e43
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-07-01 02:24:32 +0530

    update

--- DP/CutType.java
+++ DP/CutType.java
@@ -66,6 +66,35 @@ public static int MCM_DP(int[] arr,int SI,int EI,int[][] dp)
     return dp[SI][EI];
 }
 
+//Follow up -> Now cost of each multiplication is 3 units and cost of one addition is 5 units
+public static int MCM_DP_02(int[] arr,int SI,int EI,int[][] dp)
+{
+    int n = arr.length;
+    for(int gap = 1; gap < n; gap++)
+    {
+        for(int si = 0, ei = gap; ei < n; si++,ei++)
+        {
+            if(si + 1 == ei)     // ek matrix ki multiplication cost = 0
+            {
+                dp[si][ei] = 0;
+                continue;
+            }
+
+            int minAns = (int)1e9;
+            for(int cut = si+1; cut < ei; cut++)
+            {
+                int lans = dp[si][cut];
+                int rans = dp[cut][ei];
+
+                minAns = Math.min(minAns,lans + arr[si] * (3 * arr[cut] + 5(arr[cut] - 1)) * arr[ei] + rans); 
+                // now formula becomes -> 3*q (5*(q-1)) * pr
+            }
+            dp[si][ei] = minAns;
+        }
+    }
+    return dp[SI][EI];
+}
+
 public static void MCM()
 {
     int[] arr = {40,20,30,10,30};
@@ -75,7 +104,7 @@ public static void MCM()
     // for(int[] d : dp)
     // Arrays.fill(d,-1);
 
-    System.out.println(MCM_DP(arr,0,n-1,dp));
+    System.out.println(MCM_DP_02(arr,0,n-1,dp));
     print2D(dp);
 
 }
