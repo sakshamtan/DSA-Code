6b1afcffbfced2e29332663f60013d30124c15df
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-07-02 00:20:37 +0530

    update

--- DP/StringSet.java
+++ DP/StringSet.java
@@ -499,4 +499,28 @@ public String longestPalindrome(String str)
     }
     return str.substring(si,si+len);
 }
+
+//Leetcode 647 -> Palindromic Substrings -> count all palindromic substring in String s
+public int countSubstrings(String s) 
+{
+    int n = s.length();
+    boolean[][] dp = new boolean[n][n];
+    int count = 0;
+        
+    for(int gap = 0; gap < n; gap++)
+    {
+        for(int i = 0, j = gap; j < n; i++,j++)
+        {
+            if(gap == 0)
+                dp[i][j] = true;
+            else if(gap == 1)
+                dp[i][j] = s.charAt(i) == s.charAt(j) ? true : false;
+            else
+                dp[i][j] = s.charAt(i) == s.charAt(j) && dp[i+1][j-1] ? true : false;
+            if(dp[i][j])
+                count++;
+        }
+    }
+    return count;
+}
 }
