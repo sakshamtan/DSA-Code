ea31419ed797771b9238ce1770e8ec611dd7f471
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-07-03 19:41:18 +0530

    update

--- DP/CutType.java
+++ DP/CutType.java
@@ -192,6 +192,46 @@ public static void minMaxEvalution()
 
 }
 
+//Leetcode 132 -> Palindromic Partitioning II
+public int minCut_memo(String s,int si,boolean[][] isPalindrome,int[] dp)
+{
+    if(isPalindrome[si][s.length()-1])
+        return dp[si] = 0;
+    if(dp[si] != -1)
+        return dp[si];
+    int minCuts = (int)1e9;
+    for(int cut = si; cut < s.length(); cut++)
+    {
+        if(isPalindrome[si][cut])
+            minCuts = Math.min(minCuts,minCut_memo(s,cut+1,isPalindrome,dp) + 1);
+    }
+    return dp[si] = minCuts;
+}
+
+//Leetcode function
+public int minCut(String s) 
+{
+    int n = s.length();
+    int[] dp = new int[n];
+    Arrays.fill(dp,-1);
+        
+    boolean[][] isPalindrome = new boolean[n][n]; // LPS vaali boolean ki dp
+    for(int gap = 0; gap < n; gap++)
+    {
+        for(int i = 0, j = gap; j < n; i++,j++)
+        {
+            if(gap == 0)
+                isPalindrome[i][j] = true;
+            else if(gap == 1)
+                isPalindrome[i][j] = s.charAt(i) == s.charAt(j);
+            else
+                isPalindrome[i][j] = s.charAt(i) == s.charAt(j) && isPalindrome[i+1][j-1];
+        }
+    }
+        
+    return minCut_memo(s,0,isPalindrome,dp);
+}
+
 public static void main(String[] args)
 {
     // MCM();
--- recursion/RecurQuestions.cpp
+++ recursion/RecurQuestions.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-//Leetcode Q17 -> Letter Combinations of a Phone Number
+//Leetcode 17 -> Letter Combinations of a Phone Number
 vector<string> codes = {" ", " ", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 vector<string> letterCombinations(string digits)
 {
@@ -45,7 +45,7 @@ vector<string> helper(string digits)
     return myAns;
 }
 
-//Leetcode Q46 permute in an array with duplicate ans allowed.
+//Leetcode 46 -> permute in an array with duplicate ans allowed.
 vector<vector<int> > res;
 vector<int> smallAns;
 void permute_(vector<int> &arr, int count, vector<bool> &vis)
@@ -74,9 +74,8 @@ vector<vector<int> > permute(vector<int> &nums)
     permute_(nums, 0, vis);
     return res;
 }
-//======================================================
 
-//Leetcode Q47 permute_Unique
+//Leetcode 47 -> permute_Unique
 vector<vector<int> > res;
 vector<int> smallAns;
 void permute_(vector<int> &arr, int count, vector<bool> &vis)
@@ -105,7 +104,7 @@ vector<vector<int> > permute(vector<int> &nums)
     return res;
 }
 
-//Leetcode Q47 better optimized approach.//===============================
+//Leetcode 47 better optimized approach
 vector<vector<int> > res;
 vector<int> smallAns;
 void permute(vector<int> &arr, int count, vector<bool> &vis)
@@ -129,10 +128,40 @@ void permute(vector<int> &arr, int count, vector<bool> &vis)
         }
     }
 }
-vector<vector<int> > permuteUnique(vector<int> &nums)
+
+//Leetcode function
+vector<vector<int>> permuteUnique(vector<int> &nums)
 {
     sort(nums.begin(), nums.end());
     vector<bool> vis(nums.size(), false);
     permute(nums, 0, vis);
+    return res;
+}
+
+//Leetcode 39 -> Combination Sum
+void coinChange_Combi_Infi(vector<int> &arr, int tar, int idx, vector<int> &ans, vector<vector<int> > &res)
+{
+    if (tar == 0)
+    {
+        res.push_back(ans);
+        return;
+    }
+    for (int i = idx; i < arr.size(); i++)
+    {
+        if (tar - arr[i] >= 0)
+        {
+            ans.push_back(arr[i]);
+            coinChange_Combi_Infi(arr, tar - arr[i], i, ans, res);
+            ans.pop_back();
+        }
+    }
+}
+
+//Leetcode function
+vector<vector<int> > combinationSum(vector<int> &candidates, int target)
+{
+    vector<int> ans;
+    vector<vector<int> > res;
+    coinChange_Combi_Infi(candidates, target, 0, ans, res);
     return res;
 }
