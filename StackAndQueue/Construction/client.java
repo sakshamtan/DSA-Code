e188628cbc102d10685fa5853e507311c42b747c
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-07-10 19:59:32 +0530

    update

--- StackAndQueue/Construction/client.java
+++ StackAndQueue/Construction/client.java
@@ -1,7 +1,40 @@
 public class client{
 
-    public static void main(String[] args)
+//Basics of Exception Handling
+public static int divideSolve(int a,int b) throws Exception
+{
+    if(b == 0)
     {
-        
+        throw new Exception("Divisor cant be 0");
     }
+    return a / b;
+}
+
+//Exception Handling by Try Catch block
+public static int divide(int a,int b) throws Exception
+{
+    int ans = -1;
+    try{
+        ans = divideSolve(a,b);
+    }
+    catch(Exception e){
+        System.out.println("ans is INFI");
+    }
+    return ans;
+}
+
+public static void main(String[] args) throws Exception
+{
+    stack st = new stack();
+    for(int i = 0; i < 10; i++)
+    {
+        st.push(i+10);
+    }
+
+    while(st.size() != 0)
+    {
+        System.out.print(st.pop() + " ");
+    }
+
+}
 }
--- StackAndQueue/Construction/stack.java
+++ StackAndQueue/Construction/stack.java
@@ -1,4 +1,92 @@
 public class stack{
 
+    private int[] arr;
+    private int tos;
+    private int NoOfElements;
+    private int maxCapacity;
 
+    protected void initialize(int size) // so that constructor mei baar baar use ho jaaye and dynamicStack mei bhi reuse ho jaaye
+    {
+        this.arr = new int[size];
+        this.tos = -1;
+        this.NoOfElements = 0;
+        this.maxCapacity = size;
+    }
+
+    stack()
+    {
+       initialize(10);
+    }
+
+    stack(int size)
+    {
+        initialize(size);
+    }
+
+protected int capacity()
+{
+    return this.maxCapacity;
+}
+
+public int size()
+{
+    return this.NoOfElements;
+}
+
+public boolean isEmpty()
+{
+    return this.NoOfElements == 0;
+}
+
+protected void stackEmptyException() throws Exception
+{
+    if(this.NoOfElements == 0)
+    throw new Exception("StackIsEmpty");
+}
+
+protected void stackOverFlowException() throws Exception
+{
+    if(this.NoOfElements == this.maxCapacity)
+    throw new Exception("StackOverflow");
+}
+
+//Protected vaale functions are to make code more reusable and modular
+protected void push_(int data)
+{
+    this.arr[++this.tos] = data;
+    this.NoOfElements++;
+}
+
+//User public vaale functions use kr skta hai bs
+public void push(int data) throws Exception
+{
+    stackOverFlowException();
+    push_(data);
+}
+
+protected int top_()
+{
+    return this.arr[this.tos];
+}
+
+public int top() throws Exception
+{
+    stackEmptyException();
+    return top_();
+}
+
+protected int pop_()
+{
+    int rv = this.arr[this.tos];
+    this.arr[this.tos] = 0;
+    this.tos--;
+    this.NoOfElements--;
+    return rv;
+}
+
+public int pop() throws Exception
+{
+   stackEmptyException();
+   return pop_(); 
+}
 }
