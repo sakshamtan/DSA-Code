cca50f4e3f79f6be3ac1e86ea77117ad66f70c3b
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-07-14 20:43:41 +0530

    update

--- StackAndQueue/questions.java
+++ StackAndQueue/questions.java
@@ -1,10 +1,10 @@
 import java.util.Stack;
 import java.util.ArrayDeque;
 
-using namespace std;
+public class questions{
 
 //N : next , G : greater , S : smaller , R : right , L : left
-void NGOR(int[] arr,int[] ans)
+public static void NGOR(int[] arr,int[] ans)
 {
     int n = arr.length;
     Arrays.fill(ans,n);
@@ -21,7 +21,7 @@ void NGOR(int[] arr,int[] ans)
 }
 
 //Next Greater on Left -> so bs for loop ki direction opposite krni hai
-void NGOL(int[] arr,int[] ans)
+public static void NGOL(int[] arr,int[] ans)
 {
     int n = arr.length;
     Arrays.fill(ans,-1);
@@ -38,7 +38,7 @@ void NGOL(int[] arr,int[] ans)
 }
 
 //Next smaller on right -> bs comparison ka sign change krna hai in NGOR mei
-void NSOR(int[] arr,int[] ans)
+public static void NSOR(int[] arr,int[] ans)
 {
     int n = arr.length;
     Arrays.fill(ans,n);
@@ -54,7 +54,7 @@ void NSOR(int[] arr,int[] ans)
     }
 }
 
-void NSOL(int[] arr,int[] ans)
+public static void NSOL(int[] arr,int[] ans)
 {
     int n = arr.length;
     Arrays.fill(ans,-1);
@@ -68,4 +68,33 @@ void NSOL(int[] arr,int[] ans)
         }
         st.push(i);
     }
+}
+
+//Stock Span Problem -> GFG
+public static int[] NGOL(int[] arr,int n)
+{
+    int[] NGOL = new int[n];
+    Arrays.fill(NGOL,-1);
+    Stack<Integer> st = new Stack<>();
+    for(int i = n - 1; i >= 0; i--)
+    {
+        while(st.size() != 0 && arr[st.peek()] < arr[i])
+        {
+            NGOL[st.pop()] = i;
+        }
+        st.push(i);
+    }
+    return NGOL;
+}
+
+//GFG function
+public static int[] calculateSpan(int price[], int n)
+{
+    int[] ans = NGOL(price,n);
+    for(int i = 0; i < n; i++)
+    {
+        ans[i] = i - ans[i];
+    }
+    return ans;
+}
 }
