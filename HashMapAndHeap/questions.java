08256247e843f62393dce2f883e6395def3daf5e
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-08-02 19:41:04 +0530

    update

--- HashMapAndHeap/questions.java
+++ HashMapAndHeap/questions.java
@@ -137,4 +137,30 @@ public int[] intersect(int[] nums1, int[] nums2)
         
     return res;
 }
+
+//Leetcode 128 -> Longest Consecutive Sequence
+public int longestConsecutive(int[] nums) 
+{
+    HashSet<Integer> set = new HashSet<>();
+    for(int ele : nums)
+        set.add(ele);
+        
+    int len = 0;
+    for(int ele : nums)
+    {
+        if(!set.contains(ele))
+            continue;
+            
+        int ple = ele - 1, pre = ele + 1;
+        set.remove(ele);
+            
+        while(set.contains(ple))
+            set.remove(ple--);
+        while(set.contains(pre))
+            set.remove(pre++);
+            
+        len = Math.max(len,pre - ple - 1);
+    }
+    return len;
+}
 }
