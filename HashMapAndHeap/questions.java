225309393aa638f9d8cc1cb6ef6cddf0e5f2565f
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-08-03 16:21:02 +0530

    update

--- HashMapAndHeap/questions.java
+++ HashMapAndHeap/questions.java
@@ -284,4 +284,57 @@ public int kthSmallest(int[][] matrix, int k)
     }
     return ans;
 }
+
+//Leetcode 380 -> Insert Delete Get Random O(1)
+class RandomizedSet {
+    HashMap<Integer,Integer> map;
+    ArrayList<Integer> list;
+    
+    // Initialize your data structure here
+    public RandomizedSet() 
+    {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+    }
+    
+    // Inserts a value to the set. Returns true if the set did not already contain the specified element
+    public boolean insert(int val) 
+    {
+        if(map.containsKey(val))
+            return false;
+        
+        list.add(val);
+        map.put(val,list.size()-1);
+        
+        return true;
+    }
+    
+    // Removes a value from the set. Returns true if the set contained the specified element
+    public boolean remove(int val) 
+    {
+        if(!map.containsKey(val))
+            return false;
+        
+        int idx = map.get(val);
+        int lidx = list.size()-1;
+        int lval = list.get(lidx);
+        
+        list.set(idx,lval);
+        map.put(lval,idx);
+        
+        list.remove(lidx);
+        map.remove(val);
+        
+        return true;
+    }
+    
+    // Get a random element from the set.
+    public int getRandom() 
+    {
+        Random rand = new Random();
+        int idx = rand.nextInt(list.size());
+        
+        return list.get(idx);
+    }
+}
 }
