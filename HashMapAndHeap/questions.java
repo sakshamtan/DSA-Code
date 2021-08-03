5a7974bd29a660676c9d08568f785bd52fee2fc3
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-08-03 14:09:22 +0530

    update

--- HashMapAndHeap/questions.java
+++ HashMapAndHeap/questions.java
@@ -253,4 +253,35 @@ public int[][] kClosest(int[][] points, int k)
         
     return ans;
 }
+
+//Leetcode 378 -> Kth Smallest Element in a sorted Matrix
+public int kthSmallest(int[][] matrix, int k) 
+{
+    int n = matrix.length;
+    int m = matrix[0].length;
+        
+    PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+        int r1 = a / m, c1 = a % m; // 1D ke coordinates mei daal rhe hai pq mei
+        int r2 = b / m, c2 = b % m;
+        
+        return matrix[r1][c1] - matrix[r2][c2]; // matrix ki values pr minPQ
+    });
+        
+    for(int i = 0; i < n; i++)
+        pq.add(i * m + 0);  // adding the complete first col of matrix
+        
+    int ans = 0;
+    while(k-- > 0)
+    {
+        int idx = pq.remove();
+        int r = idx / m;
+        int c = idx % m;
+            
+        ans = matrix[r][c];
+        c++;
+        if(c < m)
+            pq.add(r * m + c);
+    }
+    return ans;
+}
 }
