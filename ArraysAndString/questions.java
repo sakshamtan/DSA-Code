5ac81249fd660443436bc181636d334a72b3c522
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-08-14 17:56:32 +0530

    update

--- ArraysAndString/questions.java
+++ ArraysAndString/questions.java
@@ -743,4 +743,39 @@ public int KadanesAlgo_01_SubArray(int[] arr)
     }
     return gSum;
 }
+
+//Leetcode 1191 -> K - Concatenation Maximum Sum
+int mod = (int)1e9 + 7;
+public int KadanesAlgo(int[] arr,int k)
+{
+    long gsum = 0, csum = 0;
+    int n = arr.length;
+        
+    for(int i = 0; i < k * n; i++)
+    {
+        int ele = arr[i % n];
+        csum += ele;
+            
+        if(csum > gsum)
+            gsum = csum;
+        if(csum <= 0)
+            csum = 0;
+    }
+    return (int)(gsum % mod);
+}
+
+//Leetcode function   
+public int kConcatenationMaxSum(int[] arr, int k) 
+{
+    long prevSum = 0, sum = 0;
+    for(int i = 1; i <= 3; i++)
+    {
+        prevSum = sum;
+        sum = KadanesAlgo(arr,i);
+            
+        if(i == k)
+            return (int)sum;
+    }
+    return (int)((prevSum + (k-2) * (sum - prevSum)) % mod);
+}
 }
