854db7b2905ada71d5fb2c00057b4d041abdf776
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-08-17 02:11:33 +0530

    update

--- ArraysAndString/questions.java
+++ ArraysAndString/questions.java
@@ -801,4 +801,86 @@ public int kConcatenationMaxSum_02(int[] arr, int k)
     arraySum = arraySum < 0 ? 0 : arraySum % mod;
     return (int) Math.max(kadanesSum,maxPrefixSum + maxSuffixSum + ((k-2) * arraySum) % mod) % mod;        
 }
+
+//Maximum Sum Rectangle -> GFG
+public int maximumSumRectangle(int R, int C, int arr[][]) 
+{
+    int n = R, m = C, maxSum = -(int)1e9;
+        
+    for(int fixRow = 0; fixRow < n; fixRow++)
+    {
+        int[] colPrefixSum = new int[m];
+        for(int row = fixRow; row < n; row++)
+        {
+            for(int col = 0; col < m; col++)
+                colPrefixSum[col] += arr[row][col];
+                    
+            int sum = KadanesAlgo_02(colPrefixSum);
+            maxSum = Math.max(maxSum,sum);
+        }
+    }
+    return maxSum;
+}
+
+//Now to print the subMatrix / rectangle that has maximum sum
+pubic int[] KadanesAlgo_Generic(int[] arr)
+{
+    int cSum = 0, gSum = -(int)1e9, gsi = 0, csi = 0, gei = 0;
+    for(int i = 0; i < arr.length; i++)
+    {
+        int ele = arr[i];
+        cSum += ele;
+
+        if(ele >= cSum)
+        {
+            cSum = ele;
+            csi = i;
+        }
+
+        if(cSum > gSum)
+        {
+            gSum = cSum;
+            gsi = csi;
+            gei = i;
+        }
+    }
+    return new int[]{gSum,gsi,gei};
+}
+
+public int maximumSumRectangle_02(int R, int C, int arr[][]) 
+{
+    int n = R, m = C, maxSum = -(int)1e9;
+    int r1 = 0, c1 = 0, r2 = 0, c2 = 0;
+    
+    for(int fixRow = 0; fixRow < n; fixRow++)
+    {
+        int[] colPrefixSum = new int[m];
+        for(int row = fixRow; row < n; row++)
+        {
+            for(int col = 0; col < m; col++)
+                colPrefixSum[col] += arr[row][col];
+                    
+            int[] res = KadanesAlgo_Generic(colPrefixSum);
+            if(res[0] >= maxSum)
+            {
+                maxSum = res[0];
+                r1 = fixRow;
+                c1 = res[1]; // si of kadanes
+                r2 = row;
+                c2 = res[2]; // ei of kadanes
+            }
+        }
+    }
+
+    for(int i = r1; i <= r2; i++)  // printing matrix from coordinates (r1,c1) to (r2,c2)
+    {
+        for(int j = c1; j <= c2; j++)
+        {
+            System.out.print(arr[i][j] + " ");
+        }
+        System.out.println();
+    }
+
+    return maxSum;
+}
 }
