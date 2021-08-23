a7ad9b59d0f9b2afb2e9206e0bfaa9c74329c58e
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-08-23 19:26:10 +0530

    update

--- SearchingAndSorting/questions.java
+++ SearchingAndSorting/questions.java
@@ -88,4 +88,120 @@ public int searchInsert(int[] arr, int data)
     int insertPos = si;
     return (lastIndex >= 0 && arr[lastIndex] == data) ? lastIndex : insertPos;
 }
+
+//Inversion Count -> (GFG)
+// Returns inversion count across two Arrays and sort the array at that range(merge sort)
+public static long inversionAcrossArray(long[] arr,int l,int r,int mid,long[] sortedArray)
+{
+    int lsi = l, lei = mid;
+    int rsi = mid+1, rei = r;
+
+    int k = 0;
+    long count = 0;
+    while(lsi <= lei && rsi <= rei)
+    {
+        if(arr[lsi] > arr[rsi])
+        {
+            count += (lei - lsi + 1);
+            sortedArray[k++] = arr[rsi++];
+        }
+        else
+            sortedArray[k++] = arr[lsi++];
+    }
+
+    while(lsi <= lei) // agr dono mei se koi arr bach gya ho end mei to as it is copy
+        sortedArray[k++] = arr[lsi++];
+    while(rsi <= rei)
+        sortedArray[k++] = arr[rsi++];   
+
+    k = 0;
+    for(int i = l; i <= r; i++)
+        arr[i] = sortedArray[k++];  // sortedArr copied into original arr to make changes in the real arr.
+    
+    return count;
+}
+
+// Main recursive function
+public static long inversionCount(long[] arr,int si,int ei,long[] sortedArray)
+{
+    if(si >= ei)
+    return 0;
+
+    int mid = (si + ei) / 2;
+
+    long leftInversionCount = inversionCount(arr,si,mid,sortedArray);  // Inversion count within left subArray
+    long rightInversionCount = inversionCount(arr,mid+1,ei,sortedArray); 
+
+    return (leftInversionCount + rightInversionCount + inversionAcrossArray(arr,si,ei,mid,sortedArray));
+}
+
+//GFG function
+public static long inversionCount(long arr[], long N)
+{
+    if(N <= 1)
+    return 0;
+
+    long[] sortedArray = new long[(int)N];
+    return inversionCount(arr,0,(int)N-1,sortedArray);
+}
+
+//Leetcode 33 -> Search in Rotated Sorted Array
+public int search(int[] arr, int target) 
+{
+    int si = 0, ei = arr.length-1;
+        
+    while(si <= ei)
+    {
+        int mid = (si + ei) / 2;
+
+        if(arr[mid] == target)
+            return mid;
+        else if(arr[si] <= arr[mid])
+        {
+            if(arr[si] <= target && target < arr[mid])
+                ei = mid-1;
+            else
+                si = mid+1;
+        }
+        else
+        {
+            if(arr[mid] < target && target <= arr[ei])
+                si = mid+1;
+            else
+                ei = mid-1;
+        }
+    }
+    return -1;
+}
+
+//Leetcode 81 -> Search in Rotated Sorted Array -> Now duplicate values are also present in the arr
+public boolean search(int[] arr, int target) 
+{
+    int si = 0, ei = arr.length-1;
+        
+    while(si <= ei)
+    {
+        int mid = (si + ei) / 2;
+        
+        if(arr[mid] == target || arr[si] == target)
+            return true;
+        else if(arr[si] < arr[mid])
+        {
+            if(arr[si] <= target && target < arr[mid])
+                ei = mid-1;
+            else
+                si = mid+1;
+        }
+        else if(arr[mid] < arr[ei])
+        {
+            if(arr[mid] < target && target <= arr[ei])
+                si = mid+1;
+            else
+                ei = mid-1;
+        }
+        else
+            si++;  // worst case -> O(N) when all ele are duplicate
+    }
+    return false;
+}
 }
