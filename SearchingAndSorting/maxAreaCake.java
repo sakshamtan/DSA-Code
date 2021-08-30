eca67280cff1817fe2c2c799e62c73f90c06ea23
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-08-30 15:25:03 +0530

    update

---
+++ SearchingAndSorting/maxAreaCake.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+public class maxAreaCake{
+
+public static boolean isPossibleToServeCake(int[] radiusArray,double cakeArea,int guests)
+{
+    int g = 0;
+    for(int i = radiusArray.length-1; i >= 0; i--)
+    {
+        double area = Math.PI * radiusArray[i] * radiusArray[i];
+        g += Math.floor(area / cakeArea);
+        
+        if(g >= guests)
+        return true;
+    }
+    return false;
+}
+
+public static double maximumAreaCake(int[] radius,int guests)
+{
+    int n = radius.length;
+    double si = 0.0, ei = 1e7;
+
+    while((ei - si) > 1e-5)
+    {
+        double cakeArea = (si + ei) / 2.0;
+
+        if(!isPossibleToServeCake(radius,cakeArea,guests))
+            ei = cakeArea - 1e-5;
+        else
+            si = cakeArea;
+    }
+    return si;
+}
+
+public static void main(String[] args)
+{
+    int[] radius = new int[]{1,1,1,2,2,3};
+    System.out.println(maximumAreaCake(radius,6));
+}
+}
--- SearchingAndSorting/questions.java
+++ SearchingAndSorting/questions.java
@@ -456,4 +456,35 @@ public int shipWithinDays_02(int[] weights, int days)
     }
     return si;
 }
+
+//Max Cake Area Question
+public boolean isPossibleToServeCake(int[] radiusArray,int cakeArea,int guests)
+{
+    int g = 0;
+    for(int i = radiusArray.length-1; i >= 0; i--)
+    {
+        double area = Math.PI * radiusArray[i] * radiusArray[i];
+        g += Math.floor(area / cakeArea);
+        
+        if(g >= guests)
+        return true;
+    }
+    return false;
+}
+
+public static double maximumAreaCake(int[] radius,int guests)
+{
+    int n = radius.length;
+    double si = 0.0, ei = 1e7;
+
+    while((ei - si) > 1e-5)
+    {
+        double cakeArea = (si + ei) / 2.0;
+
+        if(!isPossibleToServeCake(radius,cakeArea,guests))
+            ei = cakeArea - 1e-5;
+        else
+            si = cakeArea;
+    }
+    return si;
 }
