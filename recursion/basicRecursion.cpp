b0bcd5fd9206ce1170ee96b8209ffa96f4358dc7
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-09-01 12:44:18 +0530

    update

--- recursion/basicRecursion.cpp
+++ recursion/basicRecursion.cpp
@@ -1,13 +1,13 @@
-#include <iostream>//rev
+#include <iostream> //rev
 using namespace std;
 
 //F-> function (n-1) ke saare dec. no.s print krdega hume bs nth no. print krna hai.
 void printDecreasing(int n)
 {
     if (n == 0)
-    return;
+        return;
 
-    cout<<n<<endl;
+    cout << n << endl;
     printDecreasing(n - 1);
 }
 
@@ -15,44 +15,42 @@ void printDecreasing(int n)
 void printIncreasing(int n)
 {
     if (n == 0)
-    return;
+        return;
 
     printIncreasing(n - 1);
-    cout<<n<<endl;
+    cout << n << endl;
 }
 
 //F-> function (n-1) ke liye dec.-incr. sequence print krdega hum nth no. seq ke pehle and last mei print krna hai.
 void printDecreasingIncreasing(int n)
 {
-    if (n==0)
+    if (n == 0)
     {
-        cout<<"BASE-> 0"<<endl;
+        cout << "BASE-> 0" << endl;
         return;
-        
     }
-        cout<<"HI-> "<<n<<endl;
-        printDecreasingIncreasing (n - 1);
-        cout<<"BYE-> "<<n<<endl;
+
+    cout << "HI-> " << n << endl;
+    printDecreasingIncreasing(n - 1);
+    cout << "BYE-> " << n << endl;
 }
 
 //same as above just even and odd check.
 void EvenOdd(int n)
-{ 
+{
     if (n == 0)
     {
-        cout<<"BASE-> 0"<<endl;
+        cout << "BASE-> 0" << endl;
         return;
-        
     }
     if (n % 2 == 0)
     {
-        cout<<"HI-> "<<n<<endl;
-
+        cout << "HI-> " << n << endl;
     }
-    EvenOdd (n - 1);
+    EvenOdd(n - 1);
     if (n % 2 != 0)
     {
-    cout<<"BYE-> "<<n<<endl;
+        cout << "BYE-> " << n << endl;
     }
 }
 
@@ -69,10 +67,10 @@ int factorial(int n)
 }
 
 //F-> function x^(n-1) ki power de dega.
-int power(int x, int n)// x^n
+int power(int x, int n) // x^n
 {
     if (n == 0)
-    return 1;
+        return 1;
 
     int pnm1 = power(x, n - 1); // pow of x^(n-1)
     int pow = x * pnm1;
@@ -80,12 +78,12 @@ int power(int x, int n)// x^n
 }
 
 //F-> function x^(n/2) ki power de dega.
-int powerBetter(int x,int n) // logn
+int powerBetter(int x, int n) // logn
 {
-    if (n == 0) 
-    return 1;
+    if (n == 0)
+        return 1;
 
-    int smallAns = powerBetter(x,n/2); //aadi calls bachayi to time and space bacha lia.
+    int smallAns = powerBetter(x, n / 2); //aadi calls bachayi to time and space bacha lia.
     smallAns *= smallAns;
     return (n % 2 != 0 ? smallAns * x : smallAns);
 }
@@ -93,7 +91,7 @@ int powerBetter(int x,int n) // logn
 int fibo(int n)
 {
     if (n <= 1)
-    return n;
+        return n;
 
     return fibo(n - 1) + fibo(n - 2);
 }
@@ -104,28 +102,27 @@ int fibo(int n)
 void tower_hanoi(string src, string dest, string help, int n)
 {
     if (n == 0)
-    return;
-    
-    tower_hanoi(src,help,dest,n-1);
-    cout<<"Move "<<n<<"th disc from "<<src<<" to "<<dest<<endl;
-    tower_hanoi(help,dest,src,n-1);
+        return;
 
+    tower_hanoi(src, help, dest, n - 1);
+    cout << "Move " << n << "th disc from " << src << " to " << dest << endl;
+    tower_hanoi(help, dest, src, n - 1);
 }
 
-int euler_01(int n,int level)
+int euler_01(int n, int level)
 {
     if (n <= 1)
     {
-        cout<<" Base: "<<n<<" @ "<<level<<endl;
+        cout << " Base: " << n << " @ " << level << endl;
         return n;
     }
-    cout<<" Pre: "<<n<<" @ "<<level<<endl;
+    cout << " Pre: " << n << " @ " << level << endl;
 
-    int a = euler_01(n-1,level+1);
-    cout<<" In: "<<n<<" @ "<<level<<endl;
-    int b = euler_01(n-2,level+1);
+    int a = euler_01(n - 1, level + 1);
+    cout << " In: " << n << " @ " << level << endl;
+    int b = euler_01(n - 2, level + 1);
 
-    cout<<" Post: "<<n<<" @ "<<level<<endl;
+    cout << " Post: " << n << " @ " << level << endl;
 
     return a + b + 3;
 }
@@ -134,25 +131,24 @@ int euler_02(int n, int level)
 {
     if (n <= 2)
     {
-        cout<<" Base: "<<n*level<<endl;
+        cout << " Base: " << n * level << endl;
         return n;
     }
-    cout<<" Pre: "<<n*level<<endl;
+    cout << " Pre: " << n * level << endl;
 
-    int a = euler_02(n-1,level+1);
-    cout<<" In1: "<<n*level<<endl;
+    int a = euler_02(n - 1, level + 1);
+    cout << " In1: " << n * level << endl;
 
-    int b = euler_02(n-2,level+1);
-    cout<<" In2: "<<n*level<<endl;
+    int b = euler_02(n - 2, level + 1);
+    cout << " In2: " << n * level << endl;
 
-    int c = euler_02(n-3,level-1);
-    cout<<" Post: "<<n*level<<endl;
+    int c = euler_02(n - 3, level - 1);
+    cout << " Post: " << n * level << endl;
 
-    return a+b+c+3;
-
+    return a + b + c + 3;
 }
 
-int main(int args,char** argv)
+int main(int args, char **argv)
 {
 
     // cout<<powerBetter(5,3);
@@ -165,7 +161,7 @@ int main(int args,char** argv)
     // DecreasingO(3);
     // tower_hanoi("A","B","C",3);
     // cout<<fibo(5);
-    cout<<euler_02(4,3);
+    cout << euler_02(4, 3);
     // cout<<euler_01(3,2);
     return 0;
 }
