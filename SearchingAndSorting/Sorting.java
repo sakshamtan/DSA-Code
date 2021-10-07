7e10e97bd1c378b29597fdba55f39902713117b1
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-10-07 16:40:12 +0530

    update

--- SearchingAndSorting/Sorting.java
+++ SearchingAndSorting/Sorting.java
@@ -9,7 +9,7 @@ public static void Bubble_Sort(int [] arr)
         for (int i = 0; i < arr.length-jc;i++)
         {
             if(arr[i] > arr[i+1])
-                    swap(arr,i,i+1);
+                swap(arr,i,i+1);
         }
         jc++;
     }
