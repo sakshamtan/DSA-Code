491516916d0b52ca370704f00b77ae9f7085cce8
Saksham Taneja <SakshamTaneja@Macs-MacBook-Pro.local>
2021-11-17 22:01:38 +0530

    update

--- Basic/NumberSystem.cpp
+++ Basic/NumberSystem.cpp
@@ -79,15 +79,15 @@ int getSum(int b, int n1, int n2)
     int res = 0;
     int carry = 0;
     int pow = 1;
-    while (n1 > 0 || n2 > 0 || c > 0)
+    while (n1 > 0 || n2 > 0 || carry > 0)
     {
         int d1 = n1 % 10;
         int d2 = n2 % 10;
         n1 = n1 / 10;
         n2 = n2 / 10;
 
-        int d = d1 + d2 + c;
-        c = d / b;
+        int d = d1 + d2 + carry;
+        carry = d / b;
         d = d % b;
 
         rv += d * p;
