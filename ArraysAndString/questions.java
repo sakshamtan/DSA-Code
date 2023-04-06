4e380fde8814febcb4563c89e20318b6b45bff44
Saksham <sakshamtaneja68@gmail.com>
2023-04-07 01:47:30 +0530

    leetcode 66

--- ArraysAndString/questions.java
+++ ArraysAndString/questions.java
@@ -63,6 +63,25 @@ public static void segregateZeroAndOne(int[] arr)
     }
 }
 
+// Leetcode 66 -> Plus One 
+public int[] plusOne(int[] digits)
+{
+    for(int i = digits.length-1; i >= 0; i--) 
+    {
+      if(digits[i] < 9) 
+      {
+        digits[i]++;
+        return digits;
+      }
+      digits[i] = 0;
+    }
+
+    // Case of [9,9,9]
+    digits = new int[digits.length + 1];
+    digits[0] = 1;
+    return digits;
+}
+
 //Leetcode 283 -> Move Zeroes
 public void moveZeroes(int[] nums) 
 {
