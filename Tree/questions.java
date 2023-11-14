d8f6af106db764bdbbf874be4dc8df8ce657a447
sakshamtan <sakshamtaneja68@gmail.com>
2023-11-15 01:26:02 +0530

    leetcode 872

--- Tree/questions.java
+++ Tree/questions.java
@@ -1650,4 +1650,29 @@ public List<TreeNode> delNodes(TreeNode root, int[] to_delete)
         
     return ans;
 }
+
+// Leetcode 872 -> Leaf-similar trees
+public void collectLeaves(TreeNode root, ArrayList<Integer> leaves) {
+    if(root == null)
+        return;
+
+    if(root.left == null && root.right == null) {  
+        leaves.add(root.val);
+    }
+
+    collectLeaves(root.left, leaves);
+    collectLeaves(root.right, leaves);
+}
+
+// Leetcode function
+public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+
+    ArrayList<Integer> leaves1 = new ArrayList<>();
+    ArrayList<Integer> leaves2 = new ArrayList<>();
+    collectLeaves(root1, leaves1);
+    collectLeaves(root2, leaves2);
+
+    return leaves1.equals(leaves2);
+    
+}
 }
